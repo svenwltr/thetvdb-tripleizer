8c4e5191cf6799f11d5c81576f5a8b9062e41d15
Sven Walter <sven@wltr.eu>
2014-06-29 18:23:12 +0200

    Improved namespace.

--- src/main/java/com/github/svenwltr/thetvdbtripleizer/Ontology.java
+++ src/main/java/com/github/svenwltr/thetvdbtripleizer/Ontology.java
@@ -9,12 +9,13 @@ public class Ontology {
 	protected static final String tvdb ="http://tvdb.wltr.eu/ontology/";
 	protected static final String rdfs ="http://www.w3.org/2000/01/rdf-schema#";
 	protected static final String dbpedia ="http://dbpedia.org/ontology/";
+	protected static final String dbpprop ="http://dbpedia.org/property/";
 	
 	private static Model m = ModelFactory.createDefaultModel();
 
 	public final static Property idProperty = m.createProperty(tvdb, "tvdbid");
 	public final static Property matchesProperty = m.createProperty(tvdb, "matches");
-	public final static Property labelProperty = m.createProperty(rdfs, "label");
+	public final static Property labelProperty = m.createProperty(dbpprop, "showName");
 	public final static Property ratingProperty = m.createProperty(tvdb, "rating");
 	public final static Property airDateProperty = m.createProperty(dbpedia, "releaseDate");
 
